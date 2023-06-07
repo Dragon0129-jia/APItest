a64593b1ea11530d57d3c262605bc815161a8d7a
DragonJia <JIAsongsong@aliyun.com>
2023-06-07 23:23:11 +0800

    Initial commit

--- testcase/t_yaml/yaml_Demo.py
+++ testcase/t_yaml/yaml_Demo.py
@@ -2,21 +2,25 @@ import os
 
 import yaml
 
+from utils.YamlUtil import YamlReader
+
 #读取单个文档
 # with open("data.yaml", "r", encoding="utf-8") as f:
 #     r = yaml.safe_load(f)
 #     print(r)
 
+re = YamlReader("./data.yaml").data()
+print(re)
+
 #读取多个文档
 
-path=os.path.join(os.path.abspath(os.path.dirname(__file__)),"data.yaml")
+reall = YamlReader("./data.yaml").data_all()
+print(reall)
 
 
-
-# with open(path, "r",encoding="utf-8") as f:
+# with open("./data.yaml", "r",encoding="utf-8") as f:
 #     r = yaml.safe_load_all(f)
 #     for i in r:
 #         print(i)
-
 
 
--- utils/YamlUtil.py
+++ utils/YamlUtil.py
@@ -4,22 +4,28 @@ import os
 
 import yaml
 
-from testcase.t_yaml.yaml_Demo import path
-
-
 class YamlReader:
 
-   def __init__(self):
-       self.path=path
-       self.data=None
-   def reader(self):
-        with open(self.path,"r",encoding="utf-8") as f:
-            self.data = yaml.safe_load(f)
-        return self.data
+   def __init__(self,yaml):
+       if os.path.exists(yaml):
+        self.yaml=yaml
+       else:
+           raise FileNotFoundError("文件不存在")
+       self._data=None
+       self._data_all=None
+   def data(self):
+        if not self._data:
+            with open(self.yaml,"rb") as f:
+                self._data = yaml.safe_load(f)
+        return self._data
+
+   def data_all(self):
+       if not self._data_all:
+           with open(self.yaml, "rb") as f:
+               self._data_all = list(yaml.safe_load_all(f))
+       return self._data_all
 
 
-if __name__ == '__main__':
-    print(YamlReader().reader())
 
 
 
